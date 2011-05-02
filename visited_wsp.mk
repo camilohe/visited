229a66508f42bd030fe07a2fddc1b92a337e382e
camilohe <camiloehe@gmail.com>
2011-05-01 21:15:40 -0500

    v0.20, first complete working version after all the changes

--- visited_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ visited - Debug ]----------
-	@"mingw32-make.exe"  -j 1 -f "visited.mk"
-clean:
-	@echo ----------Cleaning project:[ visited - Debug ]----------
-	@"mingw32-make.exe"  -j 1 -f "visited.mk" clean
